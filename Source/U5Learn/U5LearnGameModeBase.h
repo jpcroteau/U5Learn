e5386982580c6a77bb1768e2b6fb9d53bfb91f3e
syrio <>
2022-07-30 22:19:09 -0400

    import

---
+++ Source/U5Learn/U5LearnGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "U5LearnGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class U5LEARN_API AU5LearnGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
